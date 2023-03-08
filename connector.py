2328c6717ce01a566361684325b8f51f741c2e2c
jhine85 <108712684+jhine85@users.noreply.github.com>
2023-03-08 13:42:43 -0600

    added packet interval and set it to 1 second

--- connector.py
+++ connector.py
@@ -1,8 +1,12 @@
 import socket
+import time
 
 # Set the IP address and port number
 ip_address = '192.168.0.10'
 port = 7142
+
+# Set the packet interval
+packet_interval = 1000  # 1 second
 
 # Create a new TCP/IP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,14 +14,19 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to the monitor
 client_socket.connect((ip_address, port))
 
-# Send a command to the monitor
-command = 'COMMAND TO SEND'
-client_socket.send(command.encode())
+# Loop to send commands at the specified interval
+while True:
+    # Send a command to the monitor
+    command = 'COMMAND TO SEND'
+    client_socket.send(command.encode())
 
-# Receive a response from the monitor
-response = client_socket.recv(8)
-response_data = response.decode()
-print('Response received:', response_data)
+    # Receive a response from the monitor
+    response = client_socket.recv(8)
+    response_data = response.decode()
+    print('Response received:', response_data)
+
+    # Wait for the specified interval before sending the next command
+    time.sleep(packet_interval / 1000)
 
 # Close the socket
 client_socket.shutdown(socket.SHUT_RDWR)
