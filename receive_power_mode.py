f8ff41f159267cd52d330e3aef56dbf0e33f666f
jhine85 <108712684+jhine85@users.noreply.github.com>
2023-03-09 14:07:02 -0600

    initial commit for receiving power message and returning ON or OFF value

---
+++ receive_power_mode.py
@@ -0,0 +1,34 @@
+def parse_message(message):
+    # Remove any whitespace or newline characters from the message
+    message = message.strip()
+
+    # Extract the monitor ID from the message
+    monitor_id = chr(int(message[3], 16) + 48)
+
+    # Extract the message type from the message
+    message_type = message[4]
+
+    # Extract the message length from the message
+    message_length = int(message[5])
+
+    # Extract the power mode from the message
+    power_mode = message[10:14]
+
+    return {
+        'monitor_id': monitor_id,
+        'message_type': message_type,
+        'message_length': message_length,
+        'power_mode': power_mode
+    }
+
+
+def set_power_mode(message):
+    parsed_message = parse_message(message)
+    power_mode = parsed_message['power_mode']
+
+    if power_mode == '0001':
+        return 'ON'
+    elif power_mode == '0004':
+        return 'OFF'
+    else:
+        return None
